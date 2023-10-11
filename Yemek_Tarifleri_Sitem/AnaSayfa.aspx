546e20ea0f8fdedbf676189f458c031936457713
Mikdat Can Şimşek <1030521058@erciyes.edu.tr>
2023-10-11 11:11:17 +0300

    Add project files.

---
+++ Yemek_Tarifleri_Sitem/AnaSayfa.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Kullanici.Master" AutoEventWireup="true" CodeBehind="AnaSayfa.aspx.cs" Inherits="Yemek_Tarifleri_Sitem.AnaSayfa" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" runat="server" contentplaceholderid="ContentPlaceHolder1">
+    <p>
+    &nbsp;</p>
+</asp:Content>
+
---
+++ Yemek_Tarifleri_Sitem/sqlsinif.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Yemek_Tarifleri_Sitem
+{
+    public class sqlsinif
+    {
+        public SqlConnection Baglanti()
+        {
+            SqlConnection baglan = new SqlConnection(@"Data Source=MIKDAT;Initial Catalog=Dbo_yemektarifi;Integrated Security=True");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
